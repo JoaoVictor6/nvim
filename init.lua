1bc2c945d071d855f52e4e76dcc5ed8c67937983
João Victor <santosjoao.dev@gmail.com>
2022-08-22 21:28:42 -0300

    fix import order

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-require('config.settings')
-require('config.aliases')
 require('plugins.plugins')
 require("plugins.gitsigns")
-require('plugins.utils')
 require('plugins.complete')
+require('config.aliases')
+require('config.settings')
+require('plugins.utils')
 
 vim.cmd([[ set termguicolors ]])
 
